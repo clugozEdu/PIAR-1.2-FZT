28ac52014fb1513d8743de42c88d8e140b0597b2
César Lugo <clugo@fztedu.org>
2024-07-17 14:07:56 -0600

    update clugo man

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import store from "./store";
-import router from "./router";
+import store from "./redux/store";
+import router from "./routes/router";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
