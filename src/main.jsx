90b2ced7798e31866703c00931789a24df4af306
César Lugo <clugo@fztedu.org>
2024-07-17 14:25:50 -0600

    test deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import store from "./redux/store";
-import router from "./routes/router";
+import store from "../src/redux/store";
+import router from "../src/routes/Routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
